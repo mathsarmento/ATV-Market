37a0a37cd19dfb3cebe8f4892e21ee3e442b3938
Matheus Sarmento <matheussarmento02@gmail.com>
2024-10-27 20:22:38 -0300

    add array pecas

--- main.c
+++ main.c
@@ -2,21 +2,21 @@
 #include <string.h>
 #include <windows.h>
 
-struct Peca {
+typedef struct {
     int codigo;
     char descricao[100];
     char marca[50];
     char aplicacao[100];
     float preco;
     int quantidade;
-};
+} Peca;
 
-struct Cliente {
+typedef struct {
     char nome[100];
     char endereco[100];
     char telefone[15];
     char email[100];
-};
+} Cliente;
 
 void loading() {
     for (int i = 0; i < 100; i++) {
@@ -27,41 +27,93 @@ void loading() {
     printf("\n");
 }
 
-void cadastrarPeca() {
-    struct Peca p;
-    int codigoExistente = 0;
+void loadPecas(Peca pecas[100]){
+    FILE *arquivo = fopen("cadastro_pecas.txt", "r");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo!\n");
+        return;
+    }
 
-   do {
-        printf("Digite o codigo do item (Somente numeros): ");
-        scanf("%d", &p.codigo);
+    Peca p;
+    int totalPecas = 0;
+    while (fscanf(arquivo, "%d\n", &p.codigo) != EOF) {
+        fscanf(arquivo, " %[^\n]", p.descricao);
+        fscanf(arquivo, " %[^\n]", p.marca);
+        fscanf(arquivo, " %[^\n]", p.aplicacao);
+        fscanf(arquivo, "%d\n", &p.quantidade);
+        fscanf(arquivo, "%f\n", &p.preco);
 
-        FILE *arquivo = fopen("cadastro_pecas.txt", "r");
-        if (arquivo == NULL) {
-            printf("Erro ao abrir o arquivo!\n");
-            return;
+        pecas[totalPecas] = p;
+        totalPecas++;
+    }
+
+    fclose(arquivo);
+}
+
+void salvaPecas(Peca pecas[100]) {
+    FILE *arquivo = fopen("cadastro_pecas.txt", "w");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo!\n");
+        return;
+    }
+
+    for (int i = 0; i < 100; i++) {
+        if (pecas[i].codigo == 0) {
+            break;
         }
 
-        struct Peca tempPeca;
-        codigoExistente = 0;
+        fprintf(arquivo, "%d\n%s\n%s\n%s\n%d\n%.2f\n", pecas[i].codigo, pecas[i].descricao, pecas[i].marca, pecas[i].aplicacao, pecas[i].quantidade, pecas[i].preco);
+    }
 
-        while (fscanf(arquivo, "%d\n", &tempPeca.codigo) != EOF) {
-            fscanf(arquivo, " %[^\n]", tempPeca.descricao);
-            fscanf(arquivo, " %[^\n]", tempPeca.marca);
-            fscanf(arquivo, " %[^\n]", tempPeca.aplicacao);
-            fscanf(arquivo, "%d\n", &tempPeca.quantidade);
-            fscanf(arquivo, "%f\n", &tempPeca.preco);
+    fclose(arquivo);
+}
 
-            if (tempPeca.codigo == p.codigo) {
-                codigoExistente = 1;
-                printf("Erro: Ja existe uma peca com o codigo %d. Tente novamente.\n", p.codigo);
-                break;
-            }
+int lenPecas(Peca pecas[100]) {
+    int i = 0;
+    while (pecas[i].codigo != 0) {
+        i++;
+    }
+
+    return i;
+}
+
+void mostrarPeca(Peca p) {
+    printf("\nCodigo: %d\nDescricao: %s\nMarca: %s\nAplicacao: %s\nPreco: %.2f\nQuantidade: %d\n\n", 
+        p.codigo, p.descricao, p.marca, p.aplicacao, p.preco, p.quantidade);
+}
+
+void listarPecas(Peca pecas[100]) {
+    int i = 0;
+
+    while (pecas[i].codigo != 0) {
+        printf("%d - %s\n", pecas[i].codigo, pecas[i].descricao);
+        i++;
+    }
+}
+
+int checkPeca(Peca pecas[100], int codigo) {
+    for (int i = 0; i < 100; i++) {
+        if (pecas[i].codigo == codigo) {
+            return i + 1;
         }
+    }
 
-        fclose(arquivo);
-    } while (codigoExistente);
+    return 0;
+}
 
-    // Se o código for único, proceder com o cadastro
+void cadastrarPeca(Peca pecas[100]) {
+    Peca p;
+
+    printf("Digite o codigo da peca: ");
+    scanf("%d", &p.codigo);
+
+    if (checkPeca(pecas, p.codigo)) {
+        printf("Peca com o codigo %d ja cadastrada!\n", p.codigo);
+        loading();
+        cadastrarPeca(pecas);
+        return;
+    }
+
     printf("Digite a descricao da peca: ");
     scanf(" %[^\n]", p.descricao);
 
@@ -77,109 +129,72 @@ void cadastrarPeca() {
     printf("Digite o preco: ");
     scanf("%f", &p.preco);
 
-    // Gravar os dados da peça no arquivo
-    FILE *arquivo = fopen("cadastro_pecas.txt", "a");
-    if (arquivo == NULL) {
-        printf("Erro ao abrir o arquivo!\n");
+    int tamanho = lenPecas(pecas);
+    if (tamanho == 100) {
+        printf("Limite de pecas atingido!\n");
         return;
     }
-
-    fprintf(arquivo, "%d\n%s\n%s\n%s\n%d\n%.2f\n", p.codigo, p.descricao, p.marca, p.aplicacao, p.quantidade, p.preco);
-    fclose(arquivo);
-
+    
+    pecas[tamanho] = p;
+    salvaPecas(pecas);
     printf("\nPeca cadastrada e gravada no arquivo com sucesso!\n");
+    loading();
 }
 
-void alterarPeca() {
+void alterarPeca(Peca pecas[100]) {
     int codigoAlvo;
+    listarPecas(pecas);
     printf("Digite o codigo do item que deseja alterar: ");
     scanf("%d", &codigoAlvo);
 
-    FILE *arquivo = fopen("cadastro_pecas.txt", "r");
-    if (arquivo == NULL) {
-        printf("Erro ao abrir o arquivo!\n");
+    int index = checkPeca(pecas, codigoAlvo);
+    if (!index) {
+        printf("Peca com o codigo %d nao encontrada.\n", codigoAlvo);
         return;
     }
 
-    struct Peca pecas[100];
-    int totalPecas = 0;
-    int encontrado = 0;
+    int opcao;
+    printf("Peca encontrada! O que deseja alterar?\n\n");
+    mostrarPeca(pecas[index - 1]);
+    loading();
 
-    while (fscanf(arquivo, "%d\n", &pecas[totalPecas].codigo) != EOF) {
-        fscanf(arquivo, " %[^\n]", pecas[totalPecas].descricao);
-        fscanf(arquivo, " %[^\n]", pecas[totalPecas].marca);
-        fscanf(arquivo, " %[^\n]", pecas[totalPecas].aplicacao);
-        fscanf(arquivo, "%d\n", &pecas[totalPecas].quantidade);
-        fscanf(arquivo, "%f\n", &pecas[totalPecas].preco);
+    printf("\n1. Descricao\n2. Marca\n3. Aplicacao\n4. Preco\n5. Quantidade\n0. Cancelar\n");
+    printf("Escolha a opcao: ");
+    scanf("%d", &opcao);
 
-        if (pecas[totalPecas].codigo == codigoAlvo) {
-            encontrado = 1;
-            printf("\nPeca encontrada! O que deseja alterar?\n");
-            printf("1. Descricao\n2. Marca\n3. Aplicacao\n4. Preco\n5. Quantidade\n0. Cancelar\n");
-
-            int opcao;
-            printf("Escolha a opcao: ");
-            scanf("%d", &opcao);
-
-            switch (opcao) {
-                case 1:
-                    printf("Digite a nova descricao: ");
-                    scanf(" %[^\n]", pecas[totalPecas].descricao);
-                    break;
-                case 2:
-                    printf("Digite a nova marca: ");
-                    scanf(" %[^\n]", pecas[totalPecas].marca);
-                    break;
-                case 3:
-                    printf("Digite a nova aplicacao: ");
-                    scanf(" %[^\n]", pecas[totalPecas].aplicacao);
-                    break;
-                case 4:
-                    printf("Digite o novo preco: ");
-                    scanf("%f", &pecas[totalPecas].preco);
-                    break;
-                case 5: 
-                    printf("Digite a nova quantidade: ");
-                    scanf("%d", &pecas[totalPecas].quantidade);
-                    break;
-                case 0:
-                    printf("Operacao cancelada.\n");
-                    fclose(arquivo);
-                    return;
-                default:
-                    printf("Opcao invalida!\n");
-                    fclose(arquivo);
-                    return;
-            }
-        }
-
-        totalPecas++;
+    switch (opcao) {
+        case 1:
+            printf("Digite a nova descricao: ");
+            scanf(" %[^\n]", pecas[index - 1].descricao);
+            break;
+        case 2:
+            printf("Digite a nova marca: ");
+            scanf(" %[^\n]", pecas[index - 1].marca);
+            break;
+        case 3:
+            printf("Digite a nova aplicacao: ");
+            scanf(" %[^\n]", pecas[index - 1].aplicacao);
+            break;
+        case 4:
+            printf("Digite o novo preco: ");
+            scanf("%f", &pecas[index - 1].preco);
+            break;
+        case 5: 
+            printf("Digite a nova quantidade: ");
+            scanf("%d", &pecas[index - 1].quantidade);
+            break;
+        case 0:
+            printf("Operacao cancelada.\n");
+            return;
+        default:
+            printf("Opcao invalida!\n");
+            return;
     }
-
-    fclose(arquivo);
-
-    if (!encontrado) {
-        printf("\nPeca com o codigo %d nao foi encontrada.\n", codigoAlvo);
-        return;
-    }
-
-    arquivo = fopen("cadastro_pecas.txt", "w");
-    if (arquivo == NULL) {
-        printf("Erro ao abrir o arquivo!\n");
-        return;
-    }
-
-    for (int i = 0; i < totalPecas; i++) {
-        fprintf(arquivo, "%d\n%s\n%s\n%s\n%d\n%.2f\n", pecas[i].codigo, pecas[i].descricao, pecas[i].marca, pecas[i].aplicacao, pecas[i].quantidade, pecas[i].preco);
-    }
-
-    fclose(arquivo);
-
-    printf("\nPeca alterada e arquivo atualizado com sucesso!\n");
+    salvaPecas(pecas);
 }
 
 void cadastrarCliente() {
-    struct Cliente c;
+    Cliente c;
     FILE *arquivo = fopen("cadastro_clientes.bin", "ab");
     
     if (arquivo == NULL) {
@@ -199,7 +214,7 @@ void cadastrarCliente() {
     printf("Digite o email do cliente: ");
     scanf(" %[^\n]", c.email);
 
-    fwrite(&c, sizeof(struct Cliente), 1, arquivo);
+    fwrite(&c, sizeof(Cliente), 1, arquivo);
     fclose(arquivo);
 
     printf("Cliente cadastrado com sucesso!\n");
@@ -218,10 +233,10 @@ void alterarCliente() {
         return;
     }
 
-    struct Cliente c;
+    Cliente c;
     long posicao;
 
-    while (fread(&c, sizeof(struct Cliente), 1, arquivo)) {
+    while (fread(&c, sizeof(Cliente), 1, arquivo)) {
         if (strcmp(c.nome, nomeBusca) == 0) {
             encontrado = 1;
             printf("Cliente encontrado!\n");
@@ -241,9 +256,9 @@ void alterarCliente() {
             scanf(" %[^\n]", c.email);
 
             // Salvar a posição atual para sobrescrever o cliente
-            posicao = ftell(arquivo) - sizeof(struct Cliente);
+            posicao = ftell(arquivo) - sizeof(Cliente);
             fseek(arquivo, posicao, SEEK_SET);
-            fwrite(&c, sizeof(struct Cliente), 1, arquivo);
+            fwrite(&c, sizeof(Cliente), 1, arquivo);
 
             printf("Dados do cliente alterados com sucesso!\n");
             break;
@@ -257,150 +272,76 @@ void alterarCliente() {
     fclose(arquivo);
 }
 
-void listarPecas() {
-    FILE *arquivo = fopen("cadastro_pecas.txt", "r");
-    if (arquivo == NULL) {
-        printf("Erro ao abrir o arquivo!\n");
-        return;
-    }
-
-    printf("\nLista de pecas cadastradas:\n");
-    printf("Codigo - Descricao\n");
-
-    struct Peca p;
-    while (fscanf(arquivo, "%d\n", &p.codigo) != EOF) {
-        fscanf(arquivo, " %[^\n]", p.descricao);
-        fscanf(arquivo, " %[^\n]", p.marca);
-        fscanf(arquivo, " %[^\n]", p.aplicacao);
-        fscanf(arquivo, "%d\n", &p.quantidade);
-        fscanf(arquivo, "%f\n", &p.preco);
-
-        printf("%d - %s \n", p.codigo, p.descricao);
-    }
-
-    fclose(arquivo);
-}
-
-void pesquisarPeca() {
+void pesquisarPeca(Peca pecas[100]) {
     int busca;
-    listarPecas();
+    listarPecas(pecas);
 
     printf("Digite o codigo da peca que deseja buscar: ");
     scanf("%d", &busca);
 
-    FILE *arquivo = fopen("cadastro_pecas.txt", "r");
-    if (arquivo == NULL) {
-        printf("Erro ao abrir o arquivo!\n");
+    int index = checkPeca(pecas, busca);
+
+    if (!index) {
+        printf("Peca com o codigo %d nao encontrada.\n", busca);
         return;
     }
 
-    struct Peca p;
-    int encontrado = 0;
-    while (fscanf(arquivo, "%d\n", &p.codigo) != EOF) {
-        fscanf(arquivo, " %[^\n]", p.descricao);
-        fscanf(arquivo, " %[^\n]", p.marca);
-        fscanf(arquivo, " %[^\n]", p.aplicacao);
-        fscanf(arquivo, "%d\n", &p.quantidade); 
-        fscanf(arquivo, "%f\n", &p.preco);
-
-        if (p.codigo == busca) {
-            encontrado = 1;
-            printf("\nPeca encontrada:\nCodigo: %d\nDescricao: %s\nPreco: %.2f\nQuantidade: %d\n", p.codigo, p.descricao, p.preco, p.quantidade);
-            loading();
-            
-            break;
-        }
-    }
-
-    fclose(arquivo);
-
-    if (!encontrado) {
-        printf("\nPeca nao encontrada.\n");
-    }
-}
-// TODO CARRINHO DE COMPRA array e ponteiro 
-void comprarPeca() {
-    char busca[100];
-    printf("Digite o nome ou descricao da peca que deseja comprar: ");
-    scanf(" %[^\n]", busca);
-
-    FILE *arquivo = fopen("cadastro_pecas.txt", "r");
-    if (arquivo == NULL) {
-        printf("Erro ao abrir o arquivo!\n");
-        return;
-    }
-
-    struct Peca pecas[100];
-    int totalPecas = 0;
-    int encontrado = 0;
-
-    while (fscanf(arquivo, "%d\n", &pecas[totalPecas].codigo) != EOF) {
-        fscanf(arquivo, " %[^\n]", pecas[totalPecas].descricao);
-        fscanf(arquivo, " %[^\n]", pecas[totalPecas].marca);
-        fscanf(arquivo, " %[^\n]", pecas[totalPecas].aplicacao);
-        fscanf(arquivo, "%d\n", &pecas[totalPecas].quantidade);
-        fscanf(arquivo, "%f\n", &pecas[totalPecas].preco);
-        totalPecas++;
-    }
-
-    fclose(arquivo);
-
-    for (int i = 0; i < totalPecas; i++) {
-        if (strstr(pecas[i].descricao, busca) != NULL) {
-            encontrado = 1;
-            printf("\nPeca encontrada:\nCodigo: %d\nDescricao: %s\nPreco: %.2f\nQuantidade disponível: %d\n", 
-                pecas[i].codigo, pecas[i].descricao, pecas[i].preco, pecas[i].quantidade);
-
-            int quantidadeDesejada;
-            printf("Digite a quantidade que deseja comprar: ");
-            scanf("%d", &quantidadeDesejada);
-
-            if (quantidadeDesejada > pecas[i].quantidade) {
-                printf("Quantidade insuficiente para compra!\n");
-            } else {
-                float valorTotal = quantidadeDesejada * pecas[i].preco;
-                printf("Valor total da compra: %.2f\n", valorTotal);
-                
-                char confirmacao;
-                printf("Deseja confirmar a compra? (s/n): ");
-                scanf(" %c", &confirmacao);
-
-                if (confirmacao == 's' || confirmacao == 'S') {
-                    pecas[i].quantidade -= quantidadeDesejada;
-                    printf("Compra realizada com sucesso!");
-                } else {
-                    printf("Compra cancelada.\n");
-                }
-            }
-            break;
-        }
-    }
-
-    arquivo = fopen("cadastro_pecas.txt", "w");
-    if (arquivo == NULL) {
-        printf("Erro ao abrir o arquivo!\n");
-        return;
-    }
-
-    for (int i = 0; i < totalPecas; i++) {
-        fprintf(arquivo, "%d\n%s\n%s\n%s\n%d\n%.2f\n", pecas[i].codigo, pecas[i].descricao, pecas[i].marca, pecas[i].aplicacao, pecas[i].quantidade, pecas[i].preco);
-    }
-
-    fclose(arquivo);
+    mostrarPeca(pecas[index - 1]);
+    loading();
 }
 
-void menuCliente() {
+void comprarPeca(Peca pecas[100]) {
+    int codigo;
+    int index;
+
+    listarPecas(pecas);
+    printf("Digite o codigo da peca que deseja comprar ou 0 para sair: ");
+    scanf("%d", &codigo);
+
+    index = checkPeca(pecas, codigo);
+    if (!index) {
+        printf("Peca com o codigo %d nao encontrada.\n", codigo);
+        return;
+    }
+
+    int quantidadeDesejada;
+
+    printf("Peca encontrada!\n");
+    mostrarPeca(pecas[index - 1]);
+    printf("Digite a quantidade que deseja comprar: ");
+    scanf("%d", &quantidadeDesejada);
+
+    if (quantidadeDesejada > pecas[index - 1].quantidade) {
+        printf("Quantidade insuficiente para compra!\n");
+    } else {
+        float valorTotal = quantidadeDesejada * pecas[index - 1].preco;
+        printf("Valor total da compra: %.2f\n", valorTotal);
+        
+        char confirmacao;
+        printf("Deseja confirmar a compra? (s/n): ");
+        scanf(" %c", &confirmacao);
+        if (confirmacao == 's' || confirmacao == 'S') {
+            pecas[index - 1].quantidade -= quantidadeDesejada;
+            printf("Compra realizada com sucesso!");
+            salvaPecas(pecas);
+        } else {
+            printf("Compra cancelada.\n");
+        }
+    }
+}
+
+void menuCliente(Peca pecas[100]) {
     int opcao;
     do {
-        printf("\nMenu Cliente\n1. Pesquisar Peca\n2. Comprar Peca\n0. Sair\nOpcao: ");
+        printf("\nMenu Cliente\n\n1. Pesquisar Peca\n2. Comprar Peca\n0. Sair\nOpcao: ");
         scanf("%d", &opcao);
 
         switch (opcao) {
             case 1:
-                pesquisarPeca();
+                pesquisarPeca(pecas);
                 break;
             case 2:
-                comprarPeca();
+                comprarPeca(pecas);
                 break;
             case 0:
                 printf("Saindo...\n");
@@ -411,27 +352,28 @@ void menuCliente() {
     } while (opcao != 0);
 }
 
-void menuAdmin() {
+void menuAdmin(Peca pecas[100], Cliente clientes[100]) {
     int opcao;
     do {
-        printf("\nMenu Admin\n1. Cadastrar Peca\n2. Alterar Peca\n3. Pesquisar Peca\n4. Cadastrar Cliente\n5. Alterar Cliente\n0. Sair\nOpcao: ");
+        printf("\nMenu Admin\n\n1. Cadastrar Peca\n2. Alterar Peca\n3. Pesquisar Peca\n4. Cadastrar Cliente\n5. Alterar Cliente\n0. Sair\nOpcao: ");
         scanf("%d", &opcao);
-
+        loading();
+        
         switch (opcao) {
             case 1:
-                cadastrarPeca();
+                cadastrarPeca(pecas);
                 break;
             case 2:
-                alterarPeca();
+                alterarPeca(pecas);
                 break;
             case 3:
-                pesquisarPeca();
+                pesquisarPeca(pecas);
                 break;
             case 4:
-                cadastrarCliente();
+                cadastrarCliente(clientes);
                 break;
             case 5:
-                alterarCliente();
+                alterarCliente(clientes);
                 break;
             case 0:
                 printf("Saindo...\n");
@@ -444,7 +386,15 @@ void menuAdmin() {
 
 int main() {
     int escolha;
+    Peca pecas[100];
+    Cliente clientes[100];
 
+    loadPecas(pecas);
+    int i = 0;
+    // while (pecas[i].codigo != 0 || pecas[i].codigo != '\0') {
+    //     printf("%d - %s\n", pecas[i].codigo, pecas[i].descricao);
+    //     i++;
+    // }
     do {
         printf("Escolha o tipo de usuario:\n1. Admin\n2. Cliente\n0. Sair\nOpcao: ");
         scanf("%d", &escolha);
@@ -458,7 +408,7 @@ int main() {
                 scanf("%s", senhaAdmin);
 
                 if (strcmp(senhaAdmin, "admin123") == 0) {
-                    menuAdmin();
+                    menuAdmin(pecas, clientes);
                     break;
                 } else {
                     tentativas--;
@@ -470,7 +420,7 @@ int main() {
                 }
             }
         } else if (escolha == 2) {
-            menuCliente();
+            menuCliente(pecas);
         } else if (escolha == 0) {
             printf("Saindo do programa...\n");
         } else {
